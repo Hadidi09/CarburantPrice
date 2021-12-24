bbeefda0c8b38d12abd603aa6ffbdfa7821d83b3
hadidi09 <cheikhhadramy@yahoo.fr>
2021-12-24 13:17:13 +0100

    add span totale

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+// Variables
 const conso = document.getElementById('consommation')
 const distance = document.getElementById('distance')
 const price = document.getElementById('price')
-const idresult = document.getElementById('resulat')
+const idresult = document.getElementById('totale')
 const submit = document.getElementById('submit')
-
 let consoChange = 0;
 let distanceChange = 0;
 let priceChange = 0;
 
+
+// J'ecoute les évenements
 conso.addEventListener('change', (e) =>
 {
      consoChange = e.target.value;
@@ -21,21 +23,18 @@ price.addEventListener('change', (e) =>
      priceChange = e.target.value;
 })
 
-console.log(consoChange);
-console.log(distanceChange);
-console.log(priceChange);
-
 // Ma fonction qui me permet de calculer le resultat 
 const resulat = () =>
 {   
-    // 
+
+    // L/100 * distance
     let result = ((consoChange / 100) * distanceChange);
     console.log(result);
-    // 
+    // Result * price / 100 
     let total = ((result * priceChange) / 100).toFixed(2);
     console.log(total);
     // Afficher le resultat total
-    idresult.innerHTML = ` votre somme total : ${total}`;
+    idresult.innerHTML = `${total}`;
 
 }
 // Ecouter la soumission du formulaire 
